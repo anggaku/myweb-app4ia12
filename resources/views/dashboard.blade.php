91db3ba4c1d2d86d29a4f42470d319ec2854697b
angga03 <antoniusanggakurniawan@gmail.com>
2021-11-24 14:36:06 +0700

    menambahkan folder dan file pada includes dan pages

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="px-3 bg-light rounded-3">
+            <div class="container-fluid py-3">
+            <h1 class="display-5 fw-bold">Hi, welcome in your dashboard</h1>
+            <p class="col-md-8 fs-4">Dashboard ini digunakan untuk mengelola halaman website personal Anda.</p>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/pages/header.blade.php
@@ -0,0 +1,14 @@
+  <!-- Full Page Image Header with Vertically Centered Content -->
+  <header class="masthead">
+    <div class="container">
+      <div class="row align-items-center row-title">
+        <div class="col-sm-3 text-center" data-aos="fade-right" data-aos-duration="1500">
+            <img src="{{ asset('img/my-photo.png') }}" class="img-fluid my-photo" alt="my-photo">
+        </div>
+        <div class="col-sm-9 text-center" data-aos="fade-up" data-aos-duration="1500">
+            <div class="my-name">Hi, I'm <strong>Angga</strong></div>
+            <div class="caption">Gunadarma IT Lecturer</div>
+        </div>
+      </div>
+    </div>
+  </header>
